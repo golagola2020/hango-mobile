71062aeae687e7a8a5dceb7db1da878f34a5afd1
KiSuSong <8345783@naver.com>
2020-08-05 03:04:58 +0900

    [mobile/createvending]자판기 추가 기능 구현 1. 입력된 자판기 정보를 서버로 전달하도록함 데이터 형식은 아래와 같다 {"vending" : {"key1" : "value1" , "key2" : "value2" ...}}
    Signed-off-by: KiSuSong <8345783@naver.com>

---
+++ mobile/login/app/src/main/java/com/example/loginactivity/CreateVendingRequest.java
@@ -0,0 +1,27 @@
+package com.example.loginactivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class CreateVendingRequest extends StringRequest{
+    final static private String URL = "http://ec2-3-34-207-199.ap-northeast-2.compute.amazonaws.com/mobile/addvending";
+    private Map<String, String> vending_parameters= new HashMap<String, String>();;
+
+    public CreateVendingRequest(String Name, String Discription, String FullSize, String SirialNumber, Response.Listener<String> listener){
+        super (Method.POST, URL, listener, null);
+        vending_parameters.put("vending_name", Name);
+        vending_parameters.put("vending_discription", Discription);
+        vending_parameters.put("full_size", FullSize);
+        vending_parameters.put("sirial_number", SirialNumber);
+    }
+    public Map getParams() throws AuthFailureError{
+        Map<String,Map<String,String>> vending = new HashMap<String,Map<String,String>>();
+        vending.put("vending", vending_parameters);
+        return vending_parameters;
+    }
+}
