7846521117f2a956616e135c69aab10f0408fad2
Ahnhyesu <linim@naver.com>
2020-08-06 11:45:11 +0900

    [arduino/ultra] 초음파 센서 수량 변경, 자판기 음료수 10개로 변경
    Signed-off-by: Ahnhyesu <linim@naver.com>

--- arduino/three_ultraJson_accuracy/three_ultraJson_accuracy.ino
+++ arduino/three_ultraJson_accuracy/three_ultraJson_accuracy.ino
@@ -3,18 +3,17 @@
 int trig1 = 2;
 int echo1 =  3;
 
-int trig2 = 4;
-int echo2 = 5;
+int trig2 = 7;
+int echo2 = 6;
 
-int trig3 = 8;
-int echo3 =  9;
+long duration1, distance1, duration2, distance2;
 
-long duration1, distance1, duration2, distance2, duration3, distance3;
-
-int number_of_drink[3][5] = {{0,1, 2, 3, 4}, {0,5, 6, 7, 8},{0,9, 10, 11, 12}};//0은 손감지 x 상태
-int chosen_number[3] = {0}; //사용자가 선택한 음료 번호 저장할 곳
-int count[3][5] = {0};//초음파 센서에 감지되는걸 카운트해 기록하는 곳.
+String number_of_drink[2][6] = {{"non","1st", "2st", "3st", "4st", "5st"},{"non", "6st", "7st", "8st", "9st", "10st"}};//0은 손감지 x 상태
+String chosen_number[2] = {"",""}; //사용자가 선택한 음료 번호 저장할 곳
+int count[2][6] = {0};//초음파 센서에 감지되는걸 카운트해 기록하는 곳.
 int i = 0; // 정확도를 위해 돌려보는 횟수 (총 10회 정도 돌려본 뒤 선택한 버튼 확정).
+String final_check = "";
+
 
 void setup() {
   //시리얼 통신을 설정(전송속도 9600bps)
@@ -24,18 +23,18 @@ void setup() {
   //초음파 송신부-> OUTPUT, 초음파 수신부 -> INPUT
   pinMode(trig1, OUTPUT);pinMode(echo1, INPUT);
   pinMode(trig2, OUTPUT);pinMode(echo2, INPUT);
-  pinMode(trig3, OUTPUT);pinMode(echo3, INPUT);
 
 }
 
 void loop() {
-
+  
   digitalWrite(trig1, LOW);
   delayMicroseconds(2);
   digitalWrite(trig1, HIGH);
   delayMicroseconds(5);
   digitalWrite(trig1, LOW);
   duration1 = pulseIn(echo1, HIGH);
+
 
   digitalWrite(trig2, LOW);
   delayMicroseconds(2);
@@ -44,30 +43,19 @@ void loop() {
   digitalWrite(trig2, LOW);
   duration2 = pulseIn(echo2, HIGH);
 
-  digitalWrite(trig3, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trig3, HIGH);
-  delayMicroseconds(5);
-  digitalWrite(trig3, LOW);
-  duration3 = pulseIn(echo3, HIGH);
-
   //초음파는 29마이크로초 당 1센치를 이동
   //초음파의 이동 거리 = duration(왕복에 걸린시간) / 29 / 2
   distance1 = (duration1 / 2) / 29;
   distance2 = (duration2 / 2) / 29;
-  distance3 = (duration3 / 2) / 29;
 
-
-  accuracy(distance1, 0);
+  accuracy(distance1, 0); //여기서 손 센싱 회수를 더함.
   accuracy(distance2, 1);
-  accuracy(distance3, 2);
 
-
-  ultra_Json(distance1, distance2, distance3);
+  ultra_Json(distance1, distance2);
   Serial.println("");
 
-  for (int a = 0; a < 3; a++) {
-    for (int b = 0; b < 5; b++) {
+   for (int a = 0; a < 2; a++) {
+    for (int b = 0; b < 6; b++) {
       Serial.print(count[a][b]); //위치별로 손이 센서에 감지된 횟수를 보기 위한 for문 
     }
     Serial.println("");
@@ -76,27 +64,30 @@ void loop() {
   Serial.println(i);
   
   i += 1;
-  if ( i == 10) { //0~9까지 총 10회
-    i = 0;
-    choose_drink(5);//q를 입력하는데 이때 q는 열의 수
-    for (int a = 0; a < 3; a++) {
-    for (int b = 0; b < 5; b++) {
-      count[a][b]=0;//다음 음료 선택을 위해 초기화.
-      }
+
+  if ( i == 9) {
+    choose_drink(6);//q를 입력하는데 이때 q는 열의 수
+    for (int a = 0; a < 2; a++) {
+      for (int b = 0; b < 6; b++) {
+        count[a][b]=0;//다음 음료 선택을 위해 초기화.
+        }
     }
-    for (int c = 0; c<3; c++){
+    for (int c = 0; c<2; c++){
+      Serial.print("chosen_number:");
       Serial.print(chosen_number[c]); //어떤 음료가 선택 되었는지 확인 
+      Serial.println("");
     }
+
+    multi_chosen_check(2); //2 : chosen number의 크기, 음료가 복수선택이 되었는지 보여주는 함수(오류체크)
+
+    chosen_drink_Json(chosen_number[0],chosen_number[1], final_check);
     Serial.println("");
 
-    multi_chosen_check(3); //3 : chosen number의 크기, 음료가 복수선택이 되었는지 보여주는 함수(오류체크)
-
-    chosen_drink_Json(chosen_number[0],chosen_number[1],chosen_number[2]);
-    Serial.println("");
+    i = 0;
   }
-  for (int c = 0; c<3; c++){
-    chosen_number[c] = 0; //다음 음료 선택을 위해 초기화.
+  for (int c = 0; c<2; c++){
+    chosen_number[c] = "non"; //다음 음료 선택을 위해 초기화.
   }
-
+  final_check = "non";  
   delay(1000);
 }
--- arduino/three_ultraJson_accuracy/three_ultraJson_accuracy_function.ino
+++ arduino/three_ultraJson_accuracy/three_ultraJson_accuracy_function.ino
@@ -1,53 +1,62 @@
-void ultra_Json(float distance1, float distance2, float distance3) { //Json방식으로 표기
+void accuracy(float distance, int i) { //손의 위치를 카운트 하는 함수
+  if (distance <= 60 && distance >= 0) {
+    if (distance >= 0 && distance < 12)count[i][6] += 1; //제일 오른쪽 음료수
+    if (distance >= 12 && distance < 19)count[i][5] += 1;
+    if (distance >= 19 && distance < 26)count[i][4] += 1;
+    if (distance >= 26 && distance < 33)count[i][3] += 1; //제일 왼쪽 음료수
+    if (distance >= 33 && distance < 45)count[i][2] += 1;
+    if (distance >= 45 && distance < 60)count[i][1] += 1;
+  }
+  else count[i][0] += 1; //손이 감지되지 않음
+}
+
+void ultra_Json(float distance1, float distance2) { //, float distance3) { //Json방식으로 표기//* 오류 확인 부분이라 마지막엔 지움.
   StaticJsonDocument<200> doc;  //JSON변환에 필요한 버퍼 선언, 용량 200 설정
   JsonArray ultra = doc.createNestedArray("ultra");
-  ultra.add(distance1);
-  ultra.add(distance2);
-  ultra.add(distance3);
-
-  //serializeJson(ultra, Serial);
+  ultra.add(distance1); Serial.print(" ");
+  ultra.add(distance2); Serial.print(" ");
   Serial.println();
   serializeJsonPretty(doc, Serial);
 }
 
-void chosen_drink_Json(int num1, int num2, int num3) {
-  StaticJsonDocument<200> doc;
-  JsonArray chosen_drink = doc.createNestedArray("chosen drink");
-  chosen_drink.add(num1);
-  chosen_drink.add(num2);
-  chosen_drink.add(num3);
-
-  Serial.println();
-  serializeJsonPretty(doc, Serial);
-}
-
-
-void accuracy(float distance, int i){ //손의 위치를 카운트 하는 함수
-  if(distance<=32 && distance >=0){
-    if(distance>=0 && distance <11)count[i][4]+=1;      //제일 오른쪽 음료수 
-    if(distance>=11 && distance <19)count[i][3]+=1;
-    if(distance>=19 && distance <26)count[i][2]+=1;      
-    if(distance>=26 && distance <33)count[i][1]+=1; //제일 왼쪽 음료수
+void choose_drink(int q) { //어떤곳이 제일 많이 센싱되었는지 나타내는 함수
+  for (int j = 0; j < q; j++) {
+    if (count[0][j] > count[0][0]) chosen_number[0] = number_of_drink[0][j];
+    if (count[1][j] > count[1][0]) chosen_number[1] = number_of_drink[1][j];
   }
-  else count[i][0]+=1;   //손이 감지되지 않음     
 }
 
-void choose_drink(int q){ //어떤곳이 제일 많이 센싱되었는지 나타내는 함수
-  for(int j=0;j<q;j++){
-    if (count[0][j]>count[0][0]) chosen_number[0]= number_of_drink[0][j];
-    if (count[1][j]>count[1][0]) chosen_number[1]= number_of_drink[1][j];
-    if (count[2][j]>count[2][0]) chosen_number[2]= number_of_drink[2][j];
-    }  
-}
-/*
- * 센싱할 때 손을 대지 않은 부분이 센싱된다면 그건 오류.
- */
-void multi_chosen_check(int p){ // 음료수가 복수 체크되었는지 오류체킹
+void multi_chosen_check(int p) { // 음료수가 복수 체크되었는지 오류체킹
   int not_0 = 0;
-  for(int i=0;i<p;i++){
-    if(chosen_number[i] !=0) not_0+=1;
+  for (int i = 0; i < p; i++) {
+    if (chosen_number[i] != "non") not_0 += 1;
   }
 
-  if(not_0 == 1 || not_0 == 0) Serial.println("normal state");
+  if ( not_0 == 0) {
+    Serial.println("normal state");
+  }
+  else if (not_0 == 1) {
+    Serial.println("normal state");
+    if (chosen_number[0] != "non") final_check = chosen_number[0];
+    if (chosen_number[1] != "non") final_check = chosen_number[1];
+    //if(chosen_number[2] != "non") final_check = chosen_number[2];
+  }
   else Serial.println("multiple selection");
+
+  Serial.print("not_0 ==");
+  Serial.println(not_0);
+}
+
+void chosen_drink_Json(String num1, String num2, String num4) { //* 오류 확인 부분,이라 마지막엔 지움.
+  // JSON 객체 선언 (동적 메모리 할당)
+  DynamicJsonDocument json(200);
+  String jsonData = "";
+
+  json["first line"] = num1;
+  json["second line"] = num2;
+  json["selected dirnk"] = num4;
+
+  serializeJson(json, jsonData);
+
+  Serial.println(jsonData);
 }
