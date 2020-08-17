6f4e2fa7b90d5324e636cc3466a8d52236bdd311
HyesuAhn <linim@naver.com>
2020-08-17 23:44:05 +0900

    [rasp/requestDrinks] 서버에게 음료수 데이터 보내기 1. 서버와의 연결 성공 2. 서버에서 보내주는 데이터 수신 성공 3. 라즈베리파이에서 서버로 데이터 송신..*
    Signed-off-by: HyesuAhn <linim@naver.com>

--- arduino/four_ultrasonic/four_ultrasonic_py.py
+++ arduino/four_ultrasonic/four_ultrasonic_py.py
@@ -1,59 +1,137 @@
 import serial
+
 import json
 
+ 
 
+##***************************************************###
+
+import requests
+
+ 
+
+url = 'http://localhost:80/rasp/drink/read'
+
+##**************************************************###
+
+ 
 
 def main():
+
     arduinoPort="/dev/ttyACM0" #when port is changed,then you must be change too.
+
     port = serial.Serial(arduinoPort,9600)
+
     port.flushInput()
 
+ 
+
     drink_mid = 'non'
+
     hand_mid= 'non'
+
     dup = 'zero'
+
     serialNumber = '20200814042555141'
+
     i = 0
+
     while port.readable:
+
         input = port.readline()
+
         arr = input.split()
 
-        #print(arr)
+ 
 
-        dict_result = { 'hand' : hand_mid[2:-1], 'drink' : drink_mid[2:-1] ,'duplicate' : dup[2:-1], 'serialNumber' :20200814042555141 }
+        #dict_result = {'serialNumber' :20200814042555141, 'sensing' :{ 'hand' : hand_mid[2:-1], 'drink' : drink_mid[2:-1] ,'duplicate' : dup[2:-1]  }}
+
+ 
+
+        dict_result = {
+
+            'serialNumber' : '20200814042555141', #if you pull server, then serialNumber -> serial_number
+
+            'drink' : drink_mid[2:-1] #drink -> sold_position
+
+            }  
+
+ 
+
         
 
-        json_vending_result = json.dumps(dict_result) 
-        #print(type(hand_mid))
+        json_vending_result = json.dumps(dict_result)# dictionary -> json
+
         
+
+ 
 
         if i == 3:
-            #print(dict_result)
-            print(json_vending_result)
+
+            #print(json_vending_result)
+
+    
+
+###*****************************************************************###                    
+
+            x = requests.post(url, data = dict_result)
+
+            print(x.text)
+
+###*****************************************************************###
+
+            
+
             i = 0
+
         i += 1
-        #print(i)
+
+ 
 
         if str(type(arr[0])) == "<class 'bytes'>":
+
             arr[0] = str(arr[0])
+
         if str(type(arr[1])) == "<class 'bytes'>":
+
             arr[1] = str(arr[1])
+
                     
 
+ 
+
         if len(arr) == 3:
+
             head = arr[0]
+
             mid = arr[1]
+
             tail = int(arr[2])
+
             if tail is 1:
+
                 hand_head = arr[0]
+
                 hand_mid = arr[1]
+
             if tail is 2:
+
                 drink_head = arr[0]
+
                 drink_mid = arr[1]
 
+ 
+
         if len(arr) == 2:
+
             dup = arr[0]
+
             dup_tail = arr[1]
 
+ 
+
         
+
 if __name__ == "__main__":
+
     main()
