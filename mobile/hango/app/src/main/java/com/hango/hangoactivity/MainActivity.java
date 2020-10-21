75a1e5738eb18b91c89b868fb17731a2c42d85c9
KiSuSong <8345783@naver.com>
2020-10-22 00:58:49 +0900

    [mobile/license]메인페이지에 license 고지
    Signed-off-by: KiSuSong <8345783@naver.com>

--- mobile/hango/app/src/main/java/com/hango/hangoactivity/MainActivity.java
+++ mobile/hango/app/src/main/java/com/hango/hangoactivity/MainActivity.java
@@ -1,3 +1,19 @@
+/**
+*        Copyright (C) 2020 Golagola
+*
+*        Licensed under the Apache License, Version 2.0 (the "License");
+*        you may not use this file except in compliance with the License.
+*        You may obtain a copy of the License at
+*
+*        http://www.apache.org/licenses/LICENSE-2.0
+*
+*        Unless required by applicable law or agreed to in writing, software
+*        distributed under the License is distributed on an "AS IS" BASIS,
+*        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*        See the License for the specific language governing permissions and
+*        limitations under the License.
+*/
+
 package com.hango.hangoactivity;
 
 import android.content.Intent;
@@ -35,6 +51,13 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.hango.environment.Network;
+import com.hango.hangoactivity.DrinkMainActivity;
+import com.hango.hangoactivity.InfoActivity;
+import com.hango.hangoactivity.MainBackPressCloseHandler;
+import com.hango.hangoactivity.R;
+import com.hango.hangoactivity.SalesDataMainActivity;
+import com.hango.hangoactivity.VendingData;
+import com.hango.hangoactivity.VendingListAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
